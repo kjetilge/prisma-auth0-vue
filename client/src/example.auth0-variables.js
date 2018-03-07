ce5e1e090d9c8a46be50cc43de2f1faaf02cbe3e
kjetilge <kjetil@geirbo.no>
2018-03-07 21:55:23 +0100

    fix example configs

---
+++ client/src/example.auth0-variables.js
@@ -0,0 +1,8 @@
+export const AUTH_CONFIG = {
+  domain: 'your-domain.auth0.com',
+  clientID: 'npaiefu98teøhrtvfgdgw5ttyjdr',
+  redirectUri: 'http://localhost:8000/callback',
+  audience: 'https://fagfilm.eu.auth0.com/api/v2/',
+  responseType: 'token id_token',
+  scope: 'openid email name avatar'
+}
